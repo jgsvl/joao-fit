65a699bb387c65f414910b8cf6ba0ebc02223176
joaogabriel.vidal <joaogabriel.vidal@eb.mil.br>
2026-03-20 12:04:54 -0300

    adicionado FitService e FitController

--- src/main/java/com/joaofit/joaofit/controller/FitController.java
+++ src/main/java/com/joaofit/joaofit/controller/FitController.java
@@ -1,4 +1,27 @@
 package com.joaofit.joaofit.controller;
 
+import com.joaofit.joaofit.service.FitService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/fit")
 public class FitController {
+
+    private final FitService fitService;
+
+    public FitController(FitService fitService){
+        this.fitService = fitService;
+    }
+
+    @PostMapping("/upload")
+    public List<String> uploadFit(@RequestParam("file")MultipartFile file){
+        return fitService.lerArquivoFit(file);
+    }
+
 }
--- src/main/java/com/joaofit/joaofit/service/FitService.java
+++ src/main/java/com/joaofit/joaofit/service/FitService.java
@@ -1,4 +1,47 @@
 package com.joaofit.joaofit.service;
 
+import com.garmin.fit.Decode;
+import com.garmin.fit.MesgBroadcaster;
+import com.garmin.fit.RecordMesg;
+import com.garmin.fit.RecordMesgListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class FitService {
+
+    public List<String> lerArquivoFit(MultipartFile file) {
+        List<String> registros = new ArrayList<>();
+        InputStream is = null;
+        try {
+            is = file.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Decode verifica a integridade do arquivo .fit
+        Decode decoder = new Decode();
+
+        MesgBroadcaster mesgBroadcaster = new MesgBroadcaster();
+
+        // Listener para mensagens de "Record" (cada ponto de GPS/batimento)
+        mesgBroadcaster.addListener((RecordMesgListener) mesg -> {
+            String dado = String.format("Tempo: %s | Lat: %f | Long: %f | Alt: %.2fm",
+                    mesg.getTimestamp(),
+                    mesg.getPositionLat() != null ? mesg.getPositionLat() * (180.0 / Math.pow(2, 31)) : 0,
+                    mesg.getPositionLong() != null ? mesg.getPositionLong() * (180.0 / Math.pow(2, 31)) : 0,
+                    mesg.getAltitude());
+            registros.add(dado);
+        });
+
+        mesgBroadcaster.run(is);
+        return registros;
+
+    }
+
 }
