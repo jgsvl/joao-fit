3519248cdec6d5b08ab1826c0853ef4b1d6f604f
joaogabriel.vidal <joaogabriel.vidal@eb.mil.br>
2026-03-20 10:19:06 -0300

    projeto inicial

---
+++ src/main/java/com/joaofit/joaofit/JoaofitApplication.java
@@ -0,0 +1,13 @@
+package com.joaofit.joaofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaofitApplication.class, args);
+	}
+
+}
